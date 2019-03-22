bc3caebbb10e6afd4b779f31cd834a481cdb6613
Curtis Dunne <curtdunne@gmail.com>
2019-03-22 10:26:21 -0600

    added Users, controller, and view to enable adding new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,12 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
     if @user.save
       session[:user_id] = @user.id
       flash[:success] = "Welcome to the Chatroom, #{ @user.username }"
       redirect_to root_path
     else
-      flash.now[:error] = "Error adding new user. Try again."
+      flash.now[:error] = "There was a problem creating new User. Make sure all fields contain valid data and try again"
       render 'new'
     end
   end
